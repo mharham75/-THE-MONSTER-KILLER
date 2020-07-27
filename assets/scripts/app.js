6110ca65f8589d8cf9774cf517611e84f7844cc8
Md Arham <md.arham@synapse.com>
2020-07-27 20:43:31 +0530

    added JS on ATTACK button : 1st commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,27 @@
+const ATTACK_VALUE = 10;    //force with which user can atack
+
+const MONSTER_ATTACK_VALUE = 14;    //FORCE WITH WHICH MONSTER CAN ATTACK
+
+let chosenMaxValue = 100;
+let currentMonsterHealth = chosenMaxValue ;
+let currentPlayerHealth = chosenMaxValue ;
+
+adjustHealthBars(chosenMaxValue);
+
+function attackHandler(){   //attack launched 
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if(currentMonsterHealth<=0 && currentPlayerHealth>0)
+        alert('You won!');
+    else if(currentPlayerHealth <=0 && currentMonsterHealth>0)
+        alert('You Lost!');
+    else if(currentMonsterHealth<=0 && currentPlayerHealth<=0)
+        alert('You have a draw');
+
+}
+
+
+attackBtn.addEventListener('click',attackHandler);
+
